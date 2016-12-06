b87b85f3d34699758e07180151719bbd7fdcab23
Maarten Nieber <hallomaarten@yahoo.com>
2016-12-06 18:44:35 +0100

    Add commands config-get and git

---
+++ extra/standard_commands/config-get.py
@@ -0,0 +1,14 @@
+# noqa
+from dodo_commands.default_commands.standard_commands import DodoCommand
+import sys
+
+
+class Command(DodoCommand):  # noqa
+    help = ""
+    decorators = []
+
+    def add_arguments_imp(self, parser):  # noqa
+        parser.add_argument('key')
+
+    def handle_imp(self, key, **kwargs):  # noqa
+        sys.stdout.write("%s\n" % str(self.get_config(key, '')))
