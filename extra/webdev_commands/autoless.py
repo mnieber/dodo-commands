618514608d474a75514fc03d3460fd0b29003905
Maarten Nieber <hallomaarten@yahoo.com>
2016-12-29 14:04:18 +0100

    Change less-watch-compiler to autoless

--- extra/webdev_commands/autoless.py
+++ extra/webdev_commands/autoless.py
@@ -1,12 +1,22 @@
 # noqa
 from dodo_commands.defaults.commands.standard_commands import DodoCommand
+import os
 
 class Command(DodoCommand):  # noqa
     help = ""
     decorators = ['docker']
 
     def handle_imp(self, **kwargs):  # noqa
+        css_dir = os.path.join(
+            self.get_config("/ROOT/donationpage_src_dir"),
+            "wordpress-theme",
+            "css"
+        )
         self.runcmd(
-            ["less-watch-compiler", ".", "css"],
+            [
+                "autoless",
+                ".",
+                css_dir
+            ],
             cwd=self.get_config("/LESS/src_dir")
         )
