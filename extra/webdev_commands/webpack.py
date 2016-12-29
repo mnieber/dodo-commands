a0ca9f5394792592658686b2729d1ce6b1497e1d
Maarten Nieber <hallomaarten@yahoo.com>
2016-12-29 14:04:37 +0100

    Add webpack_args argument

--- extra/webdev_commands/webpack.py
+++ extra/webdev_commands/webpack.py
@@ -1,12 +1,22 @@
 """Run the webpack command."""
+import argparse
 from dodo_commands.defaults.commands.standard_commands import DodoCommand
 
 
 class Command(DodoCommand):  # noqa
     decorators = ['docker']
 
-    def handle_imp(self, **kwargs):  # noqa
+    def add_arguments_imp(self, parser):  # noqa
+        parser.add_argument(
+            '--args',
+            dest="webpack_args",
+            required=False,
+            default=[],
+            nargs=argparse.REMAINDER
+        )
+
+    def handle_imp(self, webpack_args, **kwargs):  # noqa
         self.runcmd(
-            ["webpack", "--watch-stdin"],
+            ["webpack", "--watch-stdin", webpack_args],
             cwd=self.get_config("/WEBPACK/webpack_dir")
         )
