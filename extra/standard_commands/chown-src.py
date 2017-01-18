a022313e396dbee72daea23ebb8c1ba9902ba140
Maarten Nieber <hallomaarten@yahoo.com>
2017-01-18 12:36:42 +0100

    Add chown-src command

---
+++ extra/standard_commands/chown-src.py
@@ -0,0 +1,23 @@
+# noqa
+from dodo_commands.defaults.commands.standard_commands import DodoCommand
+import os
+from plumbum.cmd import whoami
+
+
+class Command(DodoCommand):  # noqa
+    help = "Changes the owner of ${/ROOT/src_dir} to `whoami`:`whoami`"
+    decorators = []
+
+    def handle_imp(self, **kwargs):  # noqa
+        me = whoami()[:-1]
+        src_dir = self.get_config("/ROOT/src_dir")
+        self.runcmd(
+            [
+                "sudo",
+                "chown",
+                "-R",
+                "%s:%s" % (me, me),
+                os.path.basename(src_dir)
+            ],
+            cwd=os.path.dirname(src_dir)
+        )
