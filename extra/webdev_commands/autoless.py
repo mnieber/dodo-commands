b3f95bf8549a7ee1cba4d9ab8291b9e8de7422dd
Maarten Nieber <hallomaarten@yahoo.com>
2017-01-24 11:07:42 +0100

    Use ${/LESS/output_dir} in autoless command

--- extra/webdev_commands/autoless.py
+++ extra/webdev_commands/autoless.py
@@ -1,22 +1,16 @@
 # noqa
 from dodo_commands.defaults.commands.standard_commands import DodoCommand
-import os
 
 class Command(DodoCommand):  # noqa
     help = ""
     decorators = ['docker']
 
     def handle_imp(self, **kwargs):  # noqa
-        css_dir = os.path.join(
-            self.get_config("/ROOT/donationpage_src_dir"),
-            "wordpress-theme",
-            "css"
-        )
         self.runcmd(
             [
                 "autoless",
                 ".",
-                css_dir
+                self.get_config("/LESS/output_dir")
             ],
             cwd=self.get_config("/LESS/src_dir")
         )
