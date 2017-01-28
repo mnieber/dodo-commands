3f0fc8f29fec34c512fa4cf9c2ad4986854034f8
Maarten Nieber <hallomaarten@yahoo.com>
2017-01-28 10:59:18 +0100

    Remove install.py

--- bin/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-INSTALL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-virtualenv -q -p python3 $INSTALL_DIR/env
-$INSTALL_DIR/env/bin/pip install -q plumbum pudb ipython PyYAML six
-$INSTALL_DIR/env/bin/python $INSTALL_DIR/install.py "$@"
