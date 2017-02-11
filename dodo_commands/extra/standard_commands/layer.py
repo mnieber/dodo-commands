7aa601a7590dc19116455ed21bcd50e47bd80ad9
Maarten Nieber <hallomaarten@yahoo.com>
2017-02-11 09:41:13 +0100

    Print an error if layer filename not found (and --force is false)

--- dodo_commands/extra/standard_commands/layer.py
+++ dodo_commands/extra/standard_commands/layer.py
@@ -2,6 +2,8 @@
 
 from . import DodoCommand
 from dodo_commands.framework.config import ConfigIO
+from dodo_commands.framework import CommandError
+import os
 
 
 def _get_list_of_layers(layers, layer, value):
@@ -30,8 +32,18 @@ class Command(DodoCommand):  # noqa
     def add_arguments_imp(self, parser):  # noqa
         parser.add_argument('layer')
         parser.add_argument('value')
+        parser.add_argument('--force', action="store_true")
 
-    def handle_imp(self, layer, value, **kwargs):  # noqa
+    def handle_imp(self, layer, value, force, **kwargs):  # noqa
+        layer_file = os.path.join(
+            self.get_config("/ROOT/res_dir"),
+            self.get_config("/ROOT/layer_dir", ""),
+            "%s.%s.yaml" % (layer, value)
+        )
+
+        if not force and not os.path.exists(layer_file):
+            raise CommandError("Layer file %s does not exist" % layer_file)
+
         config = ConfigIO().load(load_layers=False)
         layers = config.get('ROOT', {}).get('layers', [])
         newlayers = _get_list_of_layers(layers, layer, value)
