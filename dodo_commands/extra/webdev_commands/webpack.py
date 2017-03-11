a4c50c6207297884bc360e031478188ba8eb8aa0
Maarten Nieber <hallomaarten@yahoo.com>
2017-03-11 16:12:21 +0100

    Add --watch argument to webpack command

--- dodo_commands/extra/webdev_commands/webpack.py
+++ dodo_commands/extra/webdev_commands/webpack.py
@@ -11,17 +11,19 @@ class Command(DodoCommand):  # noqa
     ]
 
     def add_arguments_imp(self, parser):  # noqa
+        parser.add_argument('--watch', action="store_true")
         parser.add_argument(
             'webpack_args',
             nargs=argparse.REMAINDER
         )
 
-    def handle_imp(self, webpack_args, **kwargs):  # noqa
+    def handle_imp(self, watch, webpack_args, **kwargs):  # noqa
         webpack = self.get_config("/WEBPACK/webpack", "webpack")
         webpack_args = webpack_args or []
 
         self.runcmd(
             [webpack]
+            + (["--watch-stdin"] if watch else [])
             + remove_trailing_dashes(webpack_args),
             cwd=self.get_config("/WEBPACK/webpack_dir")
         )
