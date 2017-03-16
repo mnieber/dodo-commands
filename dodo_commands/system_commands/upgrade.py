04b2dc5e023e3129a41ccfc56bc82a0d84256784
Maarten Nieber <hallomaarten@yahoo.com>
2017-03-16 23:17:36 +0100

    Prevent running dodo upgrade with activated dodo project

--- dodo_commands/system_commands/upgrade.py
+++ dodo_commands/system_commands/upgrade.py
@@ -1,5 +1,5 @@
 # noqa
-from dodo_commands.system_commands import DodoCommand
+from dodo_commands.system_commands import DodoCommand, CommandError
 import os
 import sys
 
@@ -9,5 +9,9 @@ class Command(DodoCommand):  # noqa
     decorators = []
 
     def handle_imp(self, **kwargs):  # noqa
+        import dodo_commands
+        dodo_commands_path = dodo_commands.__path__[0]
+        if os.path.realpath(dodo_commands_path) != dodo_commands_path:
+            raise CommandError("Please deactivate your dodo project first.")
         pip = os.path.join(os.path.dirname(sys.executable), "pip")
         self.runcmd([pip, "install", "--upgrade", "dodo_commands"])
