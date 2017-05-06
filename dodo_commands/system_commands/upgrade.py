4a9fdec964a09aa7f129963b505bfa68c795bf17
Maarten Nieber <hallomaarten@yahoo.com>
2017-05-06 13:14:39 +0200

    Add flag --sudo to dodo upgrade

--- dodo_commands/system_commands/upgrade.py
+++ dodo_commands/system_commands/upgrade.py
@@ -8,10 +8,23 @@ class Command(DodoCommand):  # noqa
     help = ""
     decorators = []
 
-    def handle_imp(self, **kwargs):  # noqa
+    def add_arguments_imp(self, parser):  # noqa
+        """
+        Entry point for subclassed commands to add custom arguments.
+        """
+        parser.add_argument(
+            '--sudo',
+            action='store_true',
+        )
+
+    def handle_imp(self, sudo, **kwargs):  # noqa
         import dodo_commands
         dodo_commands_path = dodo_commands.__path__[0]
         if os.path.realpath(dodo_commands_path) != dodo_commands_path:
             raise CommandError("Please deactivate your dodo project first.")
         pip = os.path.join(os.path.dirname(sys.executable), "pip")
-        self.runcmd([pip, "install", "--upgrade", "dodo_commands"])
+
+        if sudo:
+            self.runcmd(["sudo", pip, "install", "--upgrade", "dodo_commands"])
+        else:
+            self.runcmd([pip, "install", "--upgrade", "dodo_commands"])
