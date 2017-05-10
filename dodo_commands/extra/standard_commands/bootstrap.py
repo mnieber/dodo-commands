30fc5f59e39636f706a53bf69acb68b59cd6dd83
Maarten Nieber <hallomaarten@yahoo.com>
2017-05-10 16:53:12 +0200

    Write src_dir value to config after bootstrapping

--- dodo_commands/extra/standard_commands/bootstrap.py
+++ dodo_commands/extra/standard_commands/bootstrap.py
@@ -1,17 +1,29 @@
 """Pull the latest version of the Dodo Commands system."""
 
 from dodo_commands.system_commands import DodoCommand, CommandError
+from dodo_commands.framework.config import ConfigIO
 import glob
 import os
 import sys
 
 
 class Command(DodoCommand):  # noqa
+    safe = False
+
     def add_arguments_imp(self, parser):  # noqa
-        parser.add_argument('clone_dir')
-        parser.add_argument('project_defaults_dir')
+        parser.add_argument(
+            'src_dir',
+            help="The src directory for the bootstrapped project"
+        )
+        parser.add_argument(
+            'project_defaults_dir',
+            help="Location relative to src_dir where the default project config is stored",
+        )
         parser.add_argument('--force', dest="use_force", action="store_true")
-        parser.add_argument('--git-url', dest='git_url')
+        parser.add_argument(
+            '--git-url', dest='git_url',
+            help="Clone this repository to the src_dir location",
+        )
         parser.add_argument(
             '--depth',
             type=int,
@@ -42,18 +54,18 @@ class Command(DodoCommand):  # noqa
                     )
             self.runcmd(["cp", "-rf", filename, dest_path])
 
-    def _clone(self, clone_dir, git_url, depth, branch):
-        if os.path.exists(clone_dir):
-            print("Path already exists, not cloning into: " + clone_dir)
+    def _clone(self, src_dir, git_url, depth, branch):
+        if os.path.exists(src_dir):
+            print("Path already exists, not cloning into: " + src_dir)
         else:
             self.runcmd(
                 [
                     "git",
                     "clone",
                     git_url,
-                    os.path.basename(clone_dir)
+                    os.path.basename(src_dir)
                 ] + (["--depth", depth] if depth else []),
-                cwd=os.path.dirname(clone_dir)
+                cwd=os.path.dirname(src_dir)
             )
             if branch:
                 self.runcmd(
@@ -62,7 +74,7 @@ class Command(DodoCommand):  # noqa
                         "checkout",
                         branch
                     ],
-                    cwd=clone_dir
+                    cwd=src_dir
                 )
 
         return True
@@ -76,9 +88,14 @@ class Command(DodoCommand):  # noqa
             self.runcmd(["rm", target_dir])
         self.runcmd(["ln", "-s", project_defaults_dir, target_dir])
 
+    def _write_src_dir(self, src_dir):
+        config = ConfigIO().load(load_layers=False)
+        config['ROOT']['src_dir'] = src_dir
+        ConfigIO().save(config)
+
     def handle_imp(
         self,
-        clone_dir,
+        src_dir,
         project_defaults_dir,
         use_force,
         git_url,
@@ -87,15 +104,16 @@ class Command(DodoCommand):  # noqa
         **kwargs
     ):  # noqa
         self.project_dir = self.get_config('/ROOT/project_dir')
-        if not os.path.isabs(clone_dir):
-            clone_dir = os.path.join(self.project_dir, clone_dir)
-        project_defaults_dir = os.path.join(
-            clone_dir, project_defaults_dir
+        clone_dir = (
+            src_dir
+            if os.path.isabs(src_dir) else
+            os.path.join(self.project_dir, src_dir)
         )
 
         if git_url:
             self._clone(clone_dir, git_url, depth, branch)
 
+        project_defaults_dir = os.path.join(clone_dir, project_defaults_dir)
         if not os.path.exists(project_defaults_dir):
             self._report(
                 "Default project location %s not " +
@@ -107,3 +125,9 @@ class Command(DodoCommand):  # noqa
 
         if git_url:
             self._copy_defaults(project_defaults_dir, use_force)
+
+        self._write_src_dir(
+            src_dir
+            if os.path.isabs(src_dir) else
+            os.path.join("${/ROOT/project_dir}", src_dir)
+        )
