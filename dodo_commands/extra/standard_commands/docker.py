b4a7351271a4ba83c012d16bfc577dbcee5ead9a
Maarten Nieber <hallomaarten@yahoo.com>
2017-07-20 18:38:09 +0200

    Add name option to dodo docker command

--- dodo_commands/extra/standard_commands/docker.py
+++ dodo_commands/extra/standard_commands/docker.py
@@ -4,10 +4,18 @@ from . import DodoCommand
 
 
 class Command(DodoCommand):  # noqa
+    docker_options = []
+
     def add_arguments_imp(self, parser):  # noqa
         parser.add_argument('command', nargs='?')
+        parser.add_argument('--name')
 
-    def handle_imp(self, command, **kwargs):  # noqa
+    def handle_imp(self, command, name, **kwargs):  # noqa
+        if name:
+            self.docker_options.append(
+                ('name', name)
+            )
+
         self.runcmd(
             ["/bin/bash"] + (["-c", command] if command else []),
             cwd=self.get_config("/DOCKER/default_cwd", None))
