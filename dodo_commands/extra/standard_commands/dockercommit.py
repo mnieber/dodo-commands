f911a00765ced393373f6027dce6625daa025f04
Maarten Nieber <hallomaarten@yahoo.com>
2018-04-03 13:44:36 +0200

    Remove name argument from dodo dockercommit

--- dodo_commands/extra/standard_commands/dockercommit.py
+++ dodo_commands/extra/standard_commands/dockercommit.py
@@ -7,9 +7,6 @@ from six.moves import input as raw_input
 class Command(DodoCommand):  # noqa
     help = ""
 
-    def add_arguments_imp(self, parser):  # noqa
-        parser.add_argument('name', nargs='?')
-
     def _containers(self):
         result = []
         for line in docker("ps", "--format", "{{.ID}} {{.Names}} {{.Image}}").split('\n'):
@@ -18,20 +15,19 @@ class Command(DodoCommand):  # noqa
                 result.append(dict(name=name, cid=cid, image=image))
         return result
 
-    def handle_imp(self, name, **kwargs):  # noqa
-        if not name:
-            containers = self._containers()
+    def handle_imp(self, **kwargs):  # noqa
+        containers = self._containers()
 
-            print("0 - exit")
-            for idx, container in enumerate(containers):
-                print("%d - %s" % (idx + 1, container['name']))
+        print("0 - exit")
+        for idx, container in enumerate(containers):
+            print("%d - %s" % (idx + 1, container['name']))
 
-            print("\nSelect a container: ")
-            choice = int(raw_input()) - 1
+        print("\nSelect a container: ")
+        choice = int(raw_input()) - 1
 
-            if choice == -1:
-                return
-            container = containers[choice]
+        if choice == -1:
+            return
+        container = containers[choice]
 
         self.runcmd(
             [
