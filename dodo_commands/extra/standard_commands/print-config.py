84df7cc91598a6893b4e9aaf5015a6aa5709ec06
Maarten Nieber <hallomaarten@yahoo.com>
2018-04-05 21:23:20 +0200

    Improve print-config when key argument is used

--- dodo_commands/extra/standard_commands/print-config.py
+++ dodo_commands/extra/standard_commands/print-config.py
@@ -2,19 +2,24 @@ import ruamel.yaml
 import re
 from argparse import ArgumentParser
 from dodo_commands.framework import Dodo
+from dodo_commands.framework.config import look_up_key
 
 
 def _args():
     parser = ArgumentParser(description="Print the full configuration.")
-    parser.add_argument('--key')
+    parser.add_argument('key', nargs='?')
     args = Dodo.parse_args(parser)
+    if args.key and args.key.startswith('/'):
+        args.key = args.key[1:]
     return args
 
 
 if Dodo.is_main(__name__):
     args = _args()
     if args.key:
-        print("%s" % str(Dodo.get_config(args.key, '')))
+        print("%s" % ruamel.yaml.round_trip_dump(
+            look_up_key(Dodo.config, args.key, '')
+        ))
     else:
         content = re.sub(
             r'^([0-9_A-Z]+\:)$',
