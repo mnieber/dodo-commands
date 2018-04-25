5e4c7c06ed1367df9756da80387fbd6e9a1922fe
Maarten Nieber <hallomaarten@yahoo.com>
2018-04-25 23:43:22 +0200

    Fix and refactor config expander

--- dodo_commands/framework/config_expander.py
+++ dodo_commands/framework/config_expander.py
@@ -6,8 +6,88 @@ import sys
 
 class KeyNotFound(CommandError):
     """Raised if an xpath is not found in a configuration."""
-
     pass
+
+
+class DictKey:
+    def __init__(self, the_dict, the_key, xpath):
+        self.dict = the_dict
+        self.key = the_key
+        self.xpath = xpath
+
+    def __repr__(self):  # noqa
+        return "DK[%s]" % self.key
+
+    def warning_msg_not_expanded(self):
+        return "Unexpanded key {key} at location /{xpath}".format(
+            key=self.key,
+            xpath='/'.join([str(x) for x in self.xpath])
+        )
+
+    def create_dict_val(self, expanded_key):  # noqa
+        if expanded_key != self.key:
+            self.dict[expanded_key] = self.dict[self.key]
+            del self.dict[self.key]
+        return DictVal(self.dict, expanded_key, self.xpath)
+
+
+class DictVal:
+    def __init__(self, the_dict, the_key, xpath):
+        self.dict = the_dict
+        self.key = the_key
+        self.xpath = xpath
+
+    def get_value(self):
+        return self.dict[self.key]
+
+    def replace_value(self, new_value):
+        self.dict[self.key] = new_value
+
+    def must_eval(self):
+        return (
+            isinstance(self.xpath[-1], str) and self.xpath[-1].endswith('_EVAL') or
+            (
+                len(self.xpath) >= 2 and
+                isinstance(self.xpath[-2], str) and self.xpath[-2].endswith('_EVAL')
+            )
+        )
+
+    def warning_msg_not_expanded(self):
+        return "Unexpanded value {val} at location /{xpath}".format(
+            val=self.get_value(),
+            xpath='/'.join([str(x) for x in self.xpath])
+        )
+
+    def __repr__(self):  # noqa
+        return "DV[%s]" % self.key
+
+
+class ListVal:
+    def __init__(self, the_list, the_idx, xpath):
+        self.idx = the_idx
+        self.list = the_list
+        self.xpath = xpath
+
+    def get_value(self):
+        return self.list[self.idx]
+
+    def replace_value(self, new_value):
+        self.list[self.idx] = new_value
+
+    def must_eval(self):
+        return (
+            len(self.xpath) >= 2 and
+            isinstance(self.xpath[-2], str) and self.xpath[-2].endswith('_EVAL')
+        )
+
+    def warning_msg_not_expanded(self):
+        return "Unexpanded value {val} at location /{xpath}".format(
+            val=self.get_value(),
+            xpath='/'.join([str(x) for x in self.xpath])
+        )
+
+    def __repr__(self):  # noqa
+        return "L[%s]" % self.idx
 
 
 class Key:
@@ -30,6 +110,17 @@ class Key:
                 return node[int(subkey)]
         except:
             raise KeyNotFound("Cannot get value at %s\n" % self)
+
+    def exists(self):
+        config_node = self.config
+        for xpath_node in self.xpath:
+            if isinstance(config_node, dict) and xpath_node in config_node:
+                config_node = config_node[xpath_node]
+            elif isinstance(config_node, list) and int(xpath_node) < len(config_node):
+                config_node = config_node[int(xpath_node)]
+            else:
+                return False
+        return True
 
     def get(self):  # noqa
         node = self.config
@@ -56,170 +147,111 @@ class Key:
 class ConfigExpander:
     """Expand environment variables and references in the config."""
 
-    _regexp = r"\$\{(/[^\}]*)\}"
+    _key_regexp = r"\$\{(/[^\}]*)\}"
 
     @classmethod
-    def _is_fully_expanded(cls, key, processed_xpaths):
-        if key.xpath in processed_xpaths:
+    def _get_xpath_from_string(cls, xpath_string):
+        return tuple([ix for ix in xpath_string.split("/") if ix])
+
+    def _expand_str(self, current_str):
+        expanded_str = current_str
+        key_expressions = [x for x in re.finditer(self._key_regexp, current_str)]
+        known_strs = []
+
+        while (key_expressions):
+            changed = False
+            for key_expression in reversed(key_expressions):
+                xpath_string = key_expression.group(1)
+                xpath = self._get_xpath_from_string(xpath_string)
+
+                key = Key(self.config, xpath)
+                if key.exists():
+                    expanded_str = (
+                        expanded_str[:key_expression.start()] +
+                        str(key.get()) +
+                        expanded_str[key_expression.end():]
+                    )
+
+                    if expanded_str not in known_strs:
+                        known_strs.append(expanded_str)
+                        changed = True
+
+            if not changed:
+                return None
+
+            current_str = expanded_str
+            key_expressions = [x for x in re.finditer(self._key_regexp, current_str)]
+
+        return os.path.expandvars(expanded_str)
+
+    def _expand(self, raw_obj):
+        if isinstance(raw_obj, str):
+            return self._expand_str(raw_obj)
+
+        if isinstance(raw_obj, list):
+            return [self._expand(x) for x in raw_obj]
+
+        if isinstance(raw_obj, dict):
+            return {self._expand(k): self._expand(v) for k, v in raw_obj.items()}
+
+        return raw_obj
+
+    def _schedule_children(self, obj, todo, xpath):
+        if isinstance(obj, dict):
+            for k in obj:
+                todo.append(DictKey(obj, k, xpath + [k]))
+            return True
+        if isinstance(obj, list):
+            for idx in range(len(obj)):
+                todo.append(ListVal(obj, idx, xpath + [idx]))
             return True
 
-        value = key.get()
-        if cls._is_leaf(value):
-            return False
-
-        return all([
-            cls._is_fully_expanded(key.child(x), processed_xpaths)
-            for x in cls._subkeys_of(value)
-        ])
-
-    @classmethod
-    def _must_evaluate(cls, key):
-        last_subkey = key.xpath[-1]
-        return (
-            isinstance(last_subkey, type(str())) and
-            last_subkey.endswith("_EVAL")
-        )
-
-    @classmethod
-    def _evaluate(cls, key):
-        value = key.get()
-        if cls._is_leaf(value):
-            try:
-                key.set(eval(value))
-            except:
-                raise CommandError(
-                    "Cannot evaluate value '%s' for key %s\n" % (value, key)
+    def _eval(self, value, xpath):
+        try:
+            return eval(value)
+        except:
+            raise CommandError(
+                "Cannot evaluate {value} at location {xpath}".format(
+                    value=value,
+                    xpath=xpath
                 )
-        else:
-            for k in cls._subkeys_of(value):
-                if cls._is_leaf(value[k]):
-                    cls._evaluate(key.child(k))
-
-    @classmethod
-    def _is_leaf(cls, value):
-        return not(
-            isinstance(value, type(dict())) or
-            isinstance(value, type(list()))
-        )
-
-    @classmethod
-    def _subkeys_of(cls, value):
-        if isinstance(value, type(dict())):
-            return [k for k in value]
-        if isinstance(value, type(list())):
-            return range(len(value))
-        return []
-
-    @classmethod
-    def _key_for_match(cls, matched_str, config):
-        key = Key(config, [])
-        for index in [ix for ix in matched_str.split("/") if ix]:
-            if isinstance(key.get(), type(list())):
-                subkey = int(index)
-            else:
-                subkey = index
-            key = key.child(subkey)
-        return key
-
-    @classmethod
-    def _expand(cls, value, config, processed_xpaths):
-        matches = re.finditer(cls._regexp, value)
-        for match in reversed([m for m in matches]):
-            replacement = cls._key_for_match(match.group(1), config)
-            if replacement.xpath not in processed_xpaths:
-                return False, None
-            value = (
-                value[:match.start()] +
-                str(replacement.get()) +
-                value[match.end():]
-            )
-        return True, os.path.expandvars(value)
-
-    @classmethod
-    def _expand_value_for(cls, key, processed_xpaths):
-        value = key.get()
-        if not cls._is_leaf(value):
-            return cls._is_fully_expanded(key, processed_xpaths), None
-        if not isinstance(value, type(str())):
-            return True, None
-        return cls._expand(
-            value, key.config, processed_xpaths
-        )
-
-    @classmethod
-    def _for_each_xpath(cls, config, node, f, xpath=None):
-        if xpath is None:
-            xpath = []
-
-        for subkey in cls._subkeys_of(node):
-            value = node[subkey]
-            f(config, list(xpath) + [subkey])
-            cls._for_each_xpath(
-                config, value, f, list(xpath) + [subkey]
             )
 
-    @classmethod
-    def _expanded_key(cls, key, processed_xpaths):
-        """Return key with variable expansion on last subkey.
+    def run(self, config):  # noqa
+        nodes = []
+        self.config = config
+        self._schedule_children(config, nodes, [])
 
-        Return None if not all variables could be expanded.
-        """
-        last_subkey = key.xpath[-1]
-        if not isinstance(last_subkey, type(str())):
-            return key
-
-        is_expanded, expanded_last_subkey = cls._expand(
-            last_subkey, key.config, processed_xpaths
-        )
-        if not is_expanded:
-            return None
-        return Key(
-            key.config, key.xpath[:-1] + [expanded_last_subkey]
-        )
-
-    @classmethod
-    def _report_unexpanded_keys(cls, all_keys):
-        all_xpaths = [str(x) for x in all_keys]
-        for xpath in all_xpaths:
-            skip = False
-            for other_xpath in all_xpaths:
-                if xpath != other_xpath:
-                    if other_xpath.startswith(xpath):
-                        skip = True
-            if not skip:
-                sys.stderr.write("Warning: unexpanded key: %s\n" % xpath)
-
-    @classmethod
-    def run(cls, config):  # noqa
-        all_keys = []
-        cls._for_each_xpath(
-            config, config,
-            lambda config, xpath: all_keys.append(Key(config, xpath))
-        )
-
-        processed_xpaths = []
         changed = True
-        while all_keys and changed:
+        while len(nodes) and changed:
             changed = False
-            for key in list(all_keys):
-                expanded_key = cls._expanded_key(key, processed_xpaths)
-                if expanded_key is None:
-                    continue
+            new_nodes = list()
+            for node in nodes:
+                if isinstance(node, DictKey):
+                    expanded_key = self._expand(node.key)
+                    if expanded_key is None:
+                        new_nodes.append(node)
+                    else:
+                        changed = True
+                        new_nodes.append(node.create_dict_val(expanded_key))
+                elif isinstance(node, DictVal) or isinstance(node, ListVal):
+                    value = node.get_value()
+                    if self._schedule_children(value, new_nodes, node.xpath):
+                        changed = True
+                    else:
+                        expanded_value = self._expand(value)
+                        if expanded_value is None:
+                            new_nodes.append(node)
+                        else:
+                            changed = True
+                            node.replace_value(
+                                self._eval(expanded_value, node.xpath)
+                                if node.must_eval() else
+                                expanded_value
+                            )
+                else:
+                    raise CommandError("Should not reach here")
+            nodes = new_nodes
 
-                is_expanded, expanded_value = cls._expand_value_for(
-                    key, processed_xpaths
-                )
-
-                if is_expanded:
-                    if expanded_value is not None:
-                        expanded_key.set(expanded_value)
-                        if expanded_key.xpath != key.xpath:
-                            key.remove()
-                    if cls._must_evaluate(expanded_key):
-                        cls._evaluate(expanded_key)
-                    processed_xpaths.append(expanded_key.xpath)
-                    all_keys.remove(key)
-                    changed = True
-
-        if all_keys:
-            cls._report_unexpanded_keys(all_keys)
+        for node in nodes:
+            sys.stderr.write("Warning: %s\n" % node.warning_msg_not_expanded())
