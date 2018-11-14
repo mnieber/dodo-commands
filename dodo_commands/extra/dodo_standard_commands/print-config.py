20ff47ac6e96c32404890a33724529bb484b5682
Maarten Nieber <hallomaarten@yahoo.com>
2018-11-14 21:00:05 +0100

    Improve output when printing a single value with print-config

--- dodo_commands/extra/dodo_standard_commands/print-config.py
+++ dodo_commands/extra/dodo_standard_commands/print-config.py
@@ -17,11 +17,15 @@ def _args():
 if Dodo.is_main(__name__):
     args = _args()
     if args.key:
-        print("%s" % ruamel.yaml.round_trip_dump(
-            look_up_key(Dodo.config, args.key, '')))
+        contents = look_up_key(Dodo.config, args.key, '')
+        if isinstance(contents, str):
+            print(contents)
+        else:
+            print("%s" % ruamel.yaml.round_trip_dump(contents))
     else:
-        content = re.sub(r'^([0-9_A-Z]+\:)$',
-                         r'\n\1',
-                         ruamel.yaml.round_trip_dump(Dodo.config),
-                         flags=re.MULTILINE)
+        content = re.sub(
+            r'^([0-9_A-Z]+\:)$',
+            r'\n\1',
+            ruamel.yaml.round_trip_dump(Dodo.config),
+            flags=re.MULTILINE)
         print(re.sub(r'^\n\n', r'\n', content, flags=re.MULTILINE))
