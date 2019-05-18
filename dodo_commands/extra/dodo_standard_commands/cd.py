f1003960e27927ce0630bcace5ddb8febcb0118b
Maarten Nieber <hallomaarten@yahoo.com>
2019-05-18 14:43:21 +0200

    Add option --browse to cd.py

--- dodo_commands/extra/dodo_standard_commands/cd.py
+++ dodo_commands/extra/dodo_standard_commands/cd.py
@@ -1,11 +1,17 @@
 from argparse import ArgumentParser
-from dodo_commands.framework import Dodo
+from configparser import NoOptionError
+from dodo_commands.framework import Dodo, CommandError
+from dodo_commands.framework.config import load_global_config_parser
 import sys
 
 
 def _args():
     parser = ArgumentParser(description='Print the command line to cd to'
                             ' a folder inside the project folder.')
+    parser.add_argument('--browse',
+                        action='store_true',
+                        help='Open file browser')
+
     parser.add_argument(
         'to',
         nargs='?',
@@ -18,4 +24,15 @@ def _args():
 
 if Dodo.is_main(__name__):
     args = _args()
+
+    if args.browse:
+        try:
+            browser = load_global_config_parser().get("settings",
+                                                      "file_browser")
+        except NoOptionError:
+            raise CommandError(
+                "The file_browser option was not found in the global configuration."
+            )
+        Dodo.run([browser, args.path])
+
     sys.stdout.write("cd %s\n" % args.path)
