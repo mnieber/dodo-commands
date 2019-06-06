0af877641981a50759283ced58fef29d0e3d559f
Maarten Nieber <hallomaarten@yahoo.com>
2019-06-06 07:29:07 +0200

    Fix crash in global-config on wrong key format

--- dodo_commands/dodo_system_commands/global-config.py
+++ dodo_commands/dodo_system_commands/global-config.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-from dodo_commands.framework import Dodo
+from dodo_commands.framework import Dodo, CommandError
 from dodo_commands.framework.config import (load_global_config_parser,
                                             write_global_config_parser)
 
@@ -16,6 +16,10 @@ def _args():
 if Dodo.is_main(__name__, safe=False):
     args = _args()
     config = load_global_config_parser()
+
+    if args.key.count('.') != 1:
+        raise CommandError('The key should have the format <section>.<value>')
+
     section, key = args.key.split('.')
     if args.val:
         config.set(section, key, args.val)
