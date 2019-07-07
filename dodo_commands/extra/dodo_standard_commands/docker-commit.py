f783cbe08bbabc8be646ceb8715b95a871f0e32d
Maarten Nieber <hallomaarten@yahoo.com>
2019-07-07 11:44:45 +0200

    Add argument '--by-container-name' to docker-commit

--- dodo_commands/extra/dodo_standard_commands/docker-commit.py
+++ dodo_commands/extra/dodo_standard_commands/docker-commit.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-from dodo_commands.framework import Dodo
+from dodo_commands.framework import Dodo, CommandError
 from plumbum.cmd import docker
 from six.moves import input as raw_input
 import sys
@@ -7,6 +7,7 @@ import sys
 
 def _args():
     parser = ArgumentParser()
+    parser.add_argument('--by-container-name', dest='container_name')
     args = Dodo.parse_args(parser)
     return args
 
@@ -25,17 +26,26 @@ if Dodo.is_main(__name__):
     args = _args()
     containers = _containers()
 
-    print("0 - exit")
-    for idx, container in enumerate(containers):
-        print("%d - %s" % (idx + 1, container['name']))
+    if args.container_name:
+        filtered_containers = [
+            x for x in containers if x.get('name') == args.container_name
+        ]
+        if not filtered_containers:
+            raise CommandError("Container not found: %s" % args.container_name)
+        assert len(filtered_containers) == 1
+        container = filtered_containers[0]
+    else:
+        print("0 - exit")
+        for idx, container in enumerate(containers):
+            print("%d - %s" % (idx + 1, container['name']))
 
-    print("\nSelect a container: ")
-    choice = int(raw_input()) - 1
+        print("\nSelect a container: ")
+        choice = int(raw_input()) - 1
 
-    if choice == -1:
-        sys.exit(0)
+        if choice == -1:
+            sys.exit(0)
 
-    container = containers[choice]
+        container = containers[choice]
 
     Dodo.run([
         'docker',
