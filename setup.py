b7afc51fdeb2ce6f0082c86c138f27ded4ec1a52
Maarten Nieber <hallomaarten@yahoo.com>
2020-03-08 09:34:52 +0100

    Install private packages in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,42 @@
+import os
+import subprocess
+import sys
+from distutils.command.install_data import install_data
+
 from setuptools import setup
+
+
+class InstallPrivatePackages(install_data):
+    def _packages_dirname(self):
+        import dodo_commands
+        dirname = os.path.dirname(os.path.realpath(dodo_commands.__file__))
+        return os.path.join(dirname, *("dependencies.packages".split(".")))
+
+    def _install_packages(self, packages_dirname):
+        for dependency in [
+                'plumbum',
+                'ruamel.yaml',
+                'parsimonious',
+                'six',
+                'funcy',
+                'ansimarkup',
+                'argcomplete',
+                'semantic_version',
+        ]:
+            package_dirname = os.path.join(packages_dirname,
+                                           *dependency.split("."))
+            module_filename = package_dirname + ".py"
+            if not os.path.exists(package_dirname) and not os.path.exists(
+                    module_filename):
+                subprocess.check_call([
+                    sys.executable, '-m', 'pip', 'install', '--target',
+                    packages_dirname, dependency
+                ])
+
+    def run(self):
+        super().run()
+        self._install_packages(self.packages_dirname())
+
 
 setup(name='dodo_commands',
       version='0.29.1',
@@ -25,5 +63,6 @@ setup(name='dodo_commands',
       entry_points={'console_scripts': [
           'dodo=dodo_commands.dodo:main',
       ]},
+      cmdclass={'install_data': InstallPrivatePackages},
       install_requires=[],
       zip_safe=False)
