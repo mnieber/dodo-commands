9fabb499addf7b08a56cc04e658a49a736b07999
Maarten Nieber <hallomaarten@yahoo.com>
2020-04-13 10:15:33 +0200

    Add arg --alt to commit-config

--- dodo_commands/extra/dodo_standard_commands/commit-config.py
+++ dodo_commands/extra/dodo_standard_commands/commit-config.py
@@ -1,24 +1,30 @@
 import os
-from argparse import ArgumentParser
 
 from dodo_commands import Dodo
 from dodo_commands.framework.config import Paths
 
 
 def _args():
-    parser = ArgumentParser()
-    parser.add_argument(
-        '--message', '-m', dest='message', help='The commit message')
-    args = Dodo.parse_args(parser)
+    Dodo.parser.add_argument('--alt', help='Run an alternative git command')
+    Dodo.parser.add_argument('--message',
+                             '-m',
+                             dest='message',
+                             help='The commit message')
+    args = Dodo.parse_args()
+    args.cwd = Paths().config_dir()
     return args
 
 
 if Dodo.is_main(__name__, safe=True):
     args = _args()
 
-    if not os.path.exists(os.path.join(Paths().config_dir(), '.git')):
-        Dodo.run(['git', 'init'], cwd=Paths().config_dir())
+    if args.alt:
+        Dodo.run(['git', *args.alt.split()], cwd=args.cwd)
+    else:
+        if not os.path.exists(os.path.join(args.cwd, '.git')):
+            Dodo.run(['git', 'init'], cwd=args.cwd)
 
-    Dodo.run(['git', 'add', '-A'], cwd=Paths().config_dir())
-    Dodo.run(['git', 'commit', '-m', args.message or 'Update configuration'],
-             cwd=Paths().config_dir())
+        Dodo.run(['git', 'add', '-A'], cwd=args.cwd)
+        Dodo.run(
+            ['git', 'commit', '-m', args.message or 'Update configuration'],
+            cwd=args.cwd)
