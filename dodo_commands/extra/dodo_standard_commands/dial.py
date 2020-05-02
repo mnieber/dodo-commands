be32c2525de22dcfc06ab450f73ee4ef66a8d2c0
Maarten Nieber <hallomaarten@yahoo.com>
2020-05-02 08:29:13 +0200

    Use groups inside /DIAL

--- dodo_commands/extra/dodo_standard_commands/dial.py
+++ dodo_commands/extra/dodo_standard_commands/dial.py
@@ -5,12 +5,17 @@ from dodo_commands import Dodo
 
 def _args():
     Dodo.parser.add_argument("number")
+    Dodo.parser.add_argument("--group", "-g", default="default")
     args = Dodo.parse_args()
-    args.dirs = Dodo.get("/DIAL", [])
+    args.dirs = Dodo.get("/DIAL", {})
     return args
 
 
 if Dodo.is_main(__name__):
     args = _args()
-    dial_dir = os.path.expanduser(os.path.expandvars(args.dirs[int(args.number)]))
-    print("cd %s" % dial_dir)
+    dirs = args.dirs[args.group] if args.group else args.dirs
+
+    for k, v in dirs.items():
+        if args.number == str(k):
+            dial_dir = os.path.expanduser(os.path.expandvars(v))
+            print(dial_dir)
