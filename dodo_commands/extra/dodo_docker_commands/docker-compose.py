f44f69864de157c9bf2b6b05ba71c0a72845f96f
Maarten Nieber <hallomaarten@yahoo.com>
2021-08-06 19:26:58 +0200

    Replace --print with --cat, and add --edit to docker-compose

--- dodo_commands/extra/dodo_docker_commands/docker-compose.py
+++ dodo_commands/extra/dodo_docker_commands/docker-compose.py
@@ -3,21 +3,26 @@ import os
 from dodo_commands import Dodo
 from dodo_commands.dependencies.get import plumbum
 from dodo_commands.framework.config import expand_keys
+from dodo_commands.framework.global_config import load_global_config_parser
 from dodo_commands.framework.util import to_arg_list
 
 
 def _args():
     Dodo.parser.description = "Run docker compose"
 
-    Dodo.parser.add_argument("compose_args")
+    Dodo.parser.add_argument("compose_args", nargs="?")
     Dodo.parser.add_argument("--detach", action="store_true")
-    Dodo.parser.add_argument("--print", action="store_true")
+
+    group = Dodo.parser.add_mutually_exclusive_group()
+    group.add_argument("--cat", action="store_true")
+    group.add_argument("--edit", action="store_true")
     args = Dodo.parse_args()
 
     key = "DOCKER_COMPOSE"
     args.cwd = Dodo.get("/" + key + "/cwd")
     args.files = Dodo.get("/" + key + "/files", None)
     args.map = Dodo.get("/" + key + "/map", {})
+    args.editor = load_global_config_parser().get("settings", "editor")
     args.compose_project_name = Dodo.get(
         "/" + key + "/compose_project_name", Dodo.get("/ROOT/env_name")
     )
@@ -43,10 +48,11 @@ if Dodo.is_main(__name__, safe=True):
     file_args = get_file_args() if args.files else []
     detach_args = ["--detach"] if args.detach else []
 
-    if args.print:
+    if args.cat:
         for f in args.files:
-            print(os.path.abspath(f))
             Dodo.run(["cat", f], cwd=args.cwd)
+    elif args.edit:
+        Dodo.run([args.editor, *args.files], cwd=args.cwd)
     else:
         with plumbum.local.env(COMPOSE_PROJECT_NAME=args.compose_project_name):
             Dodo.run(
