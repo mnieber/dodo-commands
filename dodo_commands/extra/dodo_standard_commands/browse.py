62253491210366e668c328a1f2c324c35ec78aeb
Maarten Nieber <hallomaarten@yahoo.com>
2021-10-26 23:32:35 +0200

    Add --url option to browse command

--- dodo_commands/extra/dodo_standard_commands/browse.py
+++ dodo_commands/extra/dodo_standard_commands/browse.py
@@ -6,11 +6,13 @@ from dodo_commands.framework.global_config import load_global_config_parser
 
 def _args():
     parser = ArgumentParser(description="")
+    parser.add_argument("--url")
 
     # Parse the arguments.
     args = Dodo.parse_args(parser)
 
     args.file_browser = load_global_config_parser().get("settings", "file_browser")
+    args.browser = load_global_config_parser().get("settings", "browser")
     args.project_dir = Dodo.get("/ROOT/project_dir")
 
     return args
@@ -19,4 +21,9 @@ def _args():
 # Use safe=False if the script makes changes other than through Dodo.run
 if Dodo.is_main(__name__, safe=True):
     args = _args()
-    Dodo.run([args.file_browser, args.project_dir])
+
+    if args.url:
+        url = Dodo.get("/URLS/" + args.url)
+        Dodo.run([args.browser, url])
+    else:
+        Dodo.run([args.file_browser, args.project_dir])
