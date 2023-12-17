27b87606a23e2318708d6dbb8082632b50cef66d
Maarten Nieber <hallomaarten@yahoo.com>
2023-12-17 01:13:54 +0100

    Extract method get_full_container_name

--- dodo_commands/extra/dodo_docker_commands/decorators/kubectl.py
+++ dodo_commands/extra/dodo_docker_commands/decorators/kubectl.py
@@ -7,20 +7,28 @@ from dodo_commands.framework.args_tree import ArgsTreeNode
 local = plumbum.local
 
 
-def _is_tuple(x):
-    return hasattr(x, "__len__") and not isinstance(x, type(str()))
-
-
 class Decorator:  # noqa
     @classmethod
-    def merged_options(cls, get_config, command_name):
+    def merged_options(cls, get_config):
         return get_config("/KUBERNETES", {})
 
     @classmethod
-    def kubectl_node(cls, get_config, command_name, cwd):
-        merged = cls.merged_options(get_config, command_name)
+    def kubectl_node(cls, get_config):
+        full_container_name = cls.get_full_container_name(get_config)
+
+        if full_container_name:
+            return ArgsTreeNode(
+                "kubectl",
+                args=["kubectl", "exec", "-it", full_container_name, "--"],
+            )
+        else:
+            raise CommandError("No kubernetes container found")
+
+    @classmethod
+    def get_full_container_name(cls, get_config):
+        options = cls.merged_options(get_config)
         labels = [
-            f"-l={key}={value}" for key, value in merged.get("labels", {}).items()
+            f"-l={key}={value}" for key, value in options.get("labels", {}).items()
         ]
         kubectl = local["kubectl"]
 
@@ -34,16 +42,9 @@ class Decorator:  # noqa
                 "--field-selector",
                 "status.phase=Running",
             )[1:-1]
-
-            return ArgsTreeNode(
-                "kubectl",
-                args=["kubectl", "exec", "-it", full_container_name, "--"],
-            )
         else:
-            raise CommandError(
-                "No kubernetes container name in /KUBERNETES for command %s"
-                % command_name
-            )
+            full_container_name = None
+        return full_container_name
 
     def add_arguments(self, parser):  # noqa
         pass
@@ -51,7 +52,7 @@ class Decorator:  # noqa
     def modify_args(
         self, command_line_args, args_tree_root_node, cwd, env_variable_map
     ):
-        kubectl_node = self.kubectl_node(Dodo.get_config, Dodo.command_name, cwd)
+        kubectl_node = self.kubectl_node(Dodo.get_config)
 
         kubectl_node.add_child(args_tree_root_node)
         return kubectl_node, local.cwd
