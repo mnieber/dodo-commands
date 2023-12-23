22b2a5b5b94f55740bf2461c3c8569c23da836c1
Maarten Nieber <hallomaarten@yahoo.com>
2023-12-23 13:37:53 +0100

    Bump version to 0.5

--- setup.py
+++ setup.py
@@ -69,12 +69,12 @@ class InstallPrivatePackages(install):
 
 setup(
     name="dodo-commands",
-    version="0.43.1",
+    version="0.5.0",
     description="Project-aware development environments, inspired by django-manage",
     long_description="Project-aware development environments, inspired by django-manage",
     long_description_content_type="text/x-rst",
     url="https://github.com/mnieber/dodo-commands",
-    download_url="https://github.com/mnieber/dodo-commands/tarball/0.43.1",
+    download_url="https://github.com/mnieber/dodo-commands/tarball/0.5.0",
     author="Maarten Nieber",
     author_email="hallomaarten@yahoo.com",
     license="MIT",
